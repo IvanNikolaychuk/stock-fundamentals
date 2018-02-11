0b18fc04deb224aec63aa39bd5d758568d69fe81
Ivan Nikolaichuk <ivan_nikolaichuk@epam.com>
2018-02-11 16:47:22 +0100

    AnalyzeSummary constructor

--- src/main/java/com/entity/AnalyzeSummary.java
+++ src/main/java/com/entity/AnalyzeSummary.java
@@ -11,9 +11,19 @@ public class AnalyzeSummary {
     @Id
     @GeneratedValue
     private int id;
+
     private String ticker;
+
     @Enumerated(EnumType.STRING)
     private PropertyType property;
     @Enumerated(EnumType.STRING)
     private Trend trend;
+
+    public AnalyzeSummary() {}
+
+    public AnalyzeSummary(String ticker, PropertyType property, Trend trend) {
+        this.ticker = ticker;
+        this.property = property;
+        this.trend = trend;
+    }
 }
