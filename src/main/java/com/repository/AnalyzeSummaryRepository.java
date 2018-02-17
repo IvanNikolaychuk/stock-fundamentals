5add68423e8679c752fbabba8bdbfd7c7a3d11ac
Ivan Nikolaichuk <ivan_nikolaichuk@epam.com>
2018-02-17 08:43:11 +0100

    find by prop. type and ticker

--- src/main/java/com/repository/AnalyzeSummaryRepository.java
+++ src/main/java/com/repository/AnalyzeSummaryRepository.java
@@ -2,9 +2,13 @@ package com.repository;
 
 import com.entity.AnalyzeSummary;
 import com.entity.CompanyProperty;
+import com.entity.PropertyType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface AnalyzeSummaryRepository extends CrudRepository<AnalyzeSummary, Integer> {
+    List<AnalyzeSummary> findByTickerAndPropertyType(String ticker, PropertyType propertyType);
 }
